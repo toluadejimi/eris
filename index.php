709e04fbb885047f3d8d88cf4b276cac172a6241
jimmy <toluadejimi@gmail.com>
2023-01-16 17:23:30 +0100

    pol

--- index.php
+++ index.php
@@ -49,12 +49,13 @@ $app = require_once __DIR__.'/../bootstrap/app.php';
 |
 */
 
-// $kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
+/*$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
 
-// $response = $kernel->handle(
-//     $request = Illuminate\Http\Request::capture()
-// );
+$response = $kernel->handle(
+    $request = Illuminate\Http\Request::capture()
+);
 
-// $response->send();
+$response->send();
 
-// $kernel->terminate($request, $response);
+$kernel->terminate($request, $response);
+*/

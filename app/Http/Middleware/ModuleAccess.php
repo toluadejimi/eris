6e8688b4838bd8f1fb9d2d5c3c38cc8a36a501ba
jimmy <toluadejimi@gmail.com>
2023-01-16 17:42:57 +0100

    false

--- app/Http/Middleware/ModuleAccess.php
+++ app/Http/Middleware/ModuleAccess.php
@@ -20,37 +20,37 @@ class ModuleAccess extends CollegeBaseController
         $moduleStatus = false;
         if(request()->is('front_desk*')){
             if($this->getGeneralSetting()->front_desk !== 1){
-                $moduleStatus = true;
+                $moduleStatus = false;
             }
         }
 
         if(request()->is('student_staff*')){
             if($this->getGeneralSetting()->student_staff !== 1){
-                $moduleStatus = true;
+                $moduleStatus = false;
             }
         }
 
         if(request()->is('account*')){
             if($this->getGeneralSetting()->account !== 1){
-                $moduleStatus = true;
+                $moduleStatus = false;
             }
         }
 
         if(request()->is('inventory*')){
             if($this->getGeneralSetting()->inventory !== 1){
-                $moduleStatus = true;
+                $moduleStatus = false;
             }
         }
 
         if(request()->is('library*')){
             if($this->getGeneralSetting()->library !== 1){
-                $moduleStatus = true;
+                $moduleStatus = false;
             }
         }
 
         if(request()->is('attendance*')){
             if($this->getGeneralSetting()->attendance !== 1){
-                $moduleStatus = true;
+                $moduleStatus = false;
             }
         }
 
@@ -62,63 +62,63 @@ class ModuleAccess extends CollegeBaseController
 
         if(request()->is('certificate*')){
             if($this->getGeneralSetting()->certificate !== 1){
-                $moduleStatus = true;
+                $moduleStatus = false;
             }
         }
 
         if(request()->is('hostel*')){
             if($this->getGeneralSetting()->hostel !== 1){
-                $moduleStatus = true;
+                $moduleStatus = false;
             }
         }
 
         if(request()->is('transport*')){
             if($this->getGeneralSetting()->transport !== 1){
-                $moduleStatus = true;
+                $moduleStatus = false;
             }
         }
 
         if(request()->is('assignment*')){
             if($this->getGeneralSetting()->assignment !== 1){
-                $moduleStatus = true;
+                $moduleStatus = false;
             }
         }
 
         if(request()->is('download*')){
             if($this->getGeneralSetting()->upload_download !== 1){
-                $moduleStatus = true;
+                $moduleStatus = false;
             }
         }
 
         if(request()->is('meeting*')){
             if($this->getGeneralSetting()->meeting !== 1){
-                $moduleStatus = true;
+                $moduleStatus = false;
             }
         }
 
         if(request()->is('alert*')){
             if($this->getGeneralSetting()->alert !== 1){
-                $moduleStatus = true;
+                $moduleStatus = false;
             }
         }
 
         if(request()->is('academic*')){
             if($this->getGeneralSetting()->academic !== 1){
-                $moduleStatus = true;
+                $moduleStatus = false;
             }
         }
 
         if(request()->is('help*')){
             if($this->getGeneralSetting()->help !== 1){
-                $moduleStatus = true;
+                $moduleStatus = false;
             }
         }
 
         if($moduleStatus){
-            // $request->session()->flash($this->message_warning, 'Module Not Activated');
-            // abort(403);
+            //$request->session()->flash($this->message_warning, 'Module Not Activated');
+            //abort(403);
         }else{
-            //return $next($request);
+            return $next($request);
         }
 
 

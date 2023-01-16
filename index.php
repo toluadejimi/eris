1e49a864396f2b1d646129ea736363c3282dc062
jimmy <toluadejimi@gmail.com>
2023-01-16 17:25:22 +0100

    ooi

--- index.php
+++ index.php
@@ -49,13 +49,4 @@ $app = require_once __DIR__.'/../bootstrap/app.php';
 |
 */
 
-/*$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
 
-$response = $kernel->handle(
-    $request = Illuminate\Http\Request::capture()
-);
-
-$response->send();
-
-$kernel->terminate($request, $response);
-*/
